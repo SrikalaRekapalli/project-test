c33ceb8a82c5f488b5b66072c6e593a647abfef1
SrikalaRekapalli <srekapalli@sysgain.com>
2018-06-10 15:53:40 +0530

    Update iot-edge-script.sh

--- scripts/iot-edge-script.sh
+++ scripts/iot-edge-script.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
+sudo apt install -y python-pip
+sudo pip install azure
+sudo apt-get install jq -y
+sudo apt-get update
+sudo apt-get install -y libssl-dev libffi-dev python-dev build-essential
+sudo apt-get install -y nodejs-legacy
+sudo apt-get install -y npm
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
