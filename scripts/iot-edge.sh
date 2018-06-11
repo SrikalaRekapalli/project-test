16b49da1fac57c0e79bc1df3b418f6791c93ed70
SrikalaRekapalli <srekapalli@sysgain.com>
2018-06-12 00:10:07 +0530

    Update iot-edge.sh

--- scripts/iot-edge.sh
+++ scripts/iot-edge.sh
@@ -5,8 +5,8 @@ DeviceName=$3
 DeviceConfig=$4
 DeviceConfigName=$5
 TenantID=$6
-ApplicationID=$7
-Certificate=$8
+AzurePortalUsername=$7
+AzurePortalPassword=$8
 SubscriptionID=$9
 sudo apt install -y python-pip
 sudo apt install -y docker.io
@@ -18,7 +18,7 @@ sudo apt-get update
 sudo apt-get install apt-transport-https -y
 sudo apt-get update && sudo apt-get install azure-cli -y
 az extension add --name azure-cli-iot-ext
-az login --service-principal -u $ApplicationID -p $Certificate --tenant $TenantID
+az login -u $AzurePortalUsername -p $AzurePortalPassword -tenant $TenantID
 az account set --subscription $SubscriptionID
 cd /home/$UserName/
 wget https://packages.microsoft.com/repos/azure-cli/pool/main/a/azure-cli/azure-cli_2.0.32-1~wheezy_all.deb
@@ -26,7 +26,7 @@ sudo dpkg -i azure-cli_2.0.32-1~wheezy_all.deb
 # Create IoT hub Edge Device
 az iot hub device-identity create -n $IoThubName -d $DeviceName -ee
 wget $DeviceConfig
-az iot hub apply-configuration -$IoThubName -d $DeviceName -k $DeviceConfigName
+az iot hub apply-configuration -n $IoThubName -d $DeviceName -k $DeviceConfigName
 connstr=`az iot hub device-identity show-connection-string -n $IoThubName -d $DeviceName | grep '"cs":'| cut -d ":" -f2 | tr -d "\"," |tr -d " "`
 export HOME=/root
 sudo pip install -U azure-iot-edge-runtime-ctl
